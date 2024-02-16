b6cd12a16e4cd280620b297063b64d1072f2b1bc
Amit <vishamit8827@gmail.com>
2024-02-16 18:16:10 +0530

    ES-6

---
+++ js/tut1.js
@@ -0,0 +1,66 @@
+console.log("Hey Welcome to tut1.js in this tut we can learn --let and const-- variables in JavaScript");
+
+
+//-------ES-5------
+
+// Var is a Gloabal Scope Variable--
+// The Var is old Variable
+/*
+
+var name = "Amit"; 
+var name ="Mohan"; //--Redeclare value--
+name = "Champa";   //--Reasign value--
+
+console.log(name);
+
+*/
+
+// if(1==1){
+//     var period = "science";
+//     console.log(period);
+// }
+// console.log(period);
+
+
+
+
+
+
+
+//-------ES-6------
+
+// let is a Block Scope Variable--
+// The let is New Variable
+
+/*
+let name = "Junior";
+// let name = "Champa"  //--Not Redeclare valule--
+name = "Champa"  //--but Reasign valule--
+
+console.log(name);
+*/
+
+
+// if(1==1){
+//     let period = "science";
+//     console.log(period);
+// }
+// console.log(period);
+
+
+// let is a Block Scope Variable--
+// The let is New Variable
+
+/*
+const name = "Junior";
+// const name = "Champa"  //--Not Redeclare valule--
+// name = "Champa"  //--Not Reasign valule--
+
+console.log(name);
+*/
+
+if(1==1){
+    const period = "science";
+    console.log(period);
+}
+// console.log(period);
---
+++ js/tut2.js
@@ -0,0 +1,6 @@
+console.log("Hey welcome to tut2.js in this tut we can learn Template Strings/Literals")
+
+let name = "Amit";
+let object = "Popcorn";
+
+console.log(`hello ${name} you want to eat ${object}`);
